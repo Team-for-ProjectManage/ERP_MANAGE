861856e7de316bcf6bc4b63569671f44c5bd5ab6
shaozhanguo <1107619732@qq.com>
2019-12-08 14:51:36 +0800

    提交

--- src/main/java/com/project/manage/controller/UserController.java
+++ src/main/java/com/project/manage/controller/UserController.java
@@ -10,20 +10,26 @@ import org.springframework.web.servlet.ModelAndView;
 import com.project.manage.common.Result;
 import com.project.manage.service.IUserService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value = "/oa")
+@Api(description = "用户操作接口")
 public class UserController {
 
 	@Autowired
 	private IUserService userService;
 	
 	@RequestMapping(value = "/find",method = RequestMethod.GET)
+	@ApiOperation(value = "获取用户信息", notes="获取用户信息")
 	public Result findAll() {
 		return userService.findAll();
 	}
 	
 
 	@RequestMapping(value = "/hellopage",method = RequestMethod.GET)
+	@ApiOperation(value = "获取用户信息", notes="获取用户信息")
 	public ModelAndView helloPage(ModelAndView mv) {
 		mv.setViewName("index");
 		return mv;
