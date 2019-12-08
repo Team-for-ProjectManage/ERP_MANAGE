f9e4b90334bee7bbe2aa963956cf6c028c746e09
shaozhanguo <1107619732@qq.com>
2019-12-08 16:29:13 +0800

    提交测试

--- src/main/java/com/project/manage/controller/UserController.java
+++ src/main/java/com/project/manage/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.project.manage.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,14 +12,17 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.project.manage.common.Result;
 import com.project.manage.service.IUserService;
+import com.project.manage.service.impl.UserServiceImpl;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@RestController
-@RequestMapping(value = "/oa")
+@Controller
+@RequestMapping(value = "/")
 @Api(description = "用户操作接口")
 public class UserController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	private IUserService userService;
@@ -28,13 +34,10 @@ public class UserController {
 	}
 	
 
-	@RequestMapping(value = "/hellopage",method = RequestMethod.GET)
+	@RequestMapping(value = "/index",method = RequestMethod.GET)
 	@ApiOperation(value = "获取用户信息", notes="获取用户信息")
-	public ModelAndView helloPage(ModelAndView mv) {
-		mv.setViewName("index");
-		return mv;
-
+	public String indexShow() {
+		return "index1";
 	}
-	
 	
 }
